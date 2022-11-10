0c0fee96f57bc29be090baf22054f6306d06fbf6
andika.gumelar <andika2000@gmail.com>
2022-11-10 16:27:46 +0700

    UPDATE
    CI CD

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,6 +14,10 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot()
     {
+        If (env('APP_ENV') != 'local') {
+            $this->app['request']->server->set('HTTPS', true);
+        }
+
         Blade::directive('require', function ($args) {
             $args = Blade::stripParentheses($args);
             $viewBasePath = Blade::getPath();
