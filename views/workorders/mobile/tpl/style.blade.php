d29b674b32bff8d346b28a1f3c2d310cf879d6c2
deniyudi <deniyudi2@gmail.com>
2025-06-12 10:18:12 +0700

    Update: Styling list ticket mobile

--- views/workorders/mobile/tpl/style.blade.php
+++ views/workorders/mobile/tpl/style.blade.php
@@ -1,66 +1,101 @@
 <style>
-    .card-list{display: block; width: 100%; position: relative}
-    .card-list .title{
-        font-size: 17px;
-        font-weight: 500;
-        color: #333;
-        line-height: 22px;
-        display: flex;
-        -webkit-line-clamp: 2;
-        display: -webkit-box;
-        -webkit-box-orient: vertical;
-        overflow: hidden;
-        margin: 0px;
-    }
-    .card-list .small-box{
-        font-size: 10px;
-        color: #333333;
-        display: inline-block;
-        padding: 1px 5px 0px 5px;
-        margin: 3px 0px;
-        background: #EEEEEE;
-        border-radius: 5px;
-    }
-    .card-list .bright-text{color: #FFFFFF}
-    .card-list .dark-text{color: #333333}
-    .card-list .content{
-        font-size: 13px;
-        color: #666666;
-        line-height: 17px;
-        display: flex;
-        -webkit-line-clamp: 3;
-        display: -webkit-box;
-        -webkit-box-orient: vertical;
-        overflow: hidden;
-        margin: 5px 0px 45px;
-    }
-    .card-list .small-text{font-size: 10px; color: #666666}
-    .card-list .medium-text{font-size: 13px; color: #666666}
-    .card-list .footer{
-        background: #F6F6F6;
-        position: absolute;
-        bottom: 0px; left: 0px; right: 0px;
-        border-top: 1px solid #eee;
-        border-radius: 0px 0px 5px 5px;
-    }
-    .card-list .footer table{width: 100%; height: 100%;}
-    .card-list .footer .status{
-        padding: 3px 0px 3px 15px;
-        color: #fff;
-        display: inline-block;
-        font-size: 10px;
-        width: 60px;
-        margin-right: 20px;
-    }
-    .card-list .assign{
-        font-size: 11px;
-        border-left: 1px solid #EEEEEE;
-        border-right: 1px solid #EEEEEE;
-        line-height: 14px;
-        padding: 10px 10px 10px 20px;
-    }
+  .card-list {
+    display: block;
+    width: 100%;
+    position: relative
+  }
 
-    .popover__content{width: 275px;}
+  .card-list .title {
+    font-size: 17px;
+    font-weight: 500;
+    color: #333;
+    line-height: 22px;
+    display: flex;
+    -webkit-line-clamp: 2;
+    display: -webkit-box;
+    -webkit-box-orient: vertical;
+    overflow: hidden;
+    margin: 0px;
+  }
 
-    .wo-detail .btn{width: 100%}
+  .card-list .small-box {
+    font-size: 10px;
+    color: #333333;
+    display: inline-block;
+    padding: 1px 5px 0px 5px;
+    margin: 3px 0px;
+    background: #EEEEEE;
+    border-radius: 5px;
+  }
+
+  .card-list .bright-text {
+    color: #FFFFFF
+  }
+
+  .card-list .dark-text {
+    color: #333333
+  }
+
+  .card-list .content {
+    font-size: 13px;
+    color: #666666;
+    line-height: 17px;
+    display: flex;
+    -webkit-line-clamp: 3;
+    display: -webkit-box;
+    -webkit-box-orient: vertical;
+    overflow: hidden;
+    margin: 5px 0px 45px;
+    max-width: 320px;
+  }
+
+  .card-list .small-text {
+    font-size: 10px;
+    color: #666666
+  }
+
+  .card-list .medium-text {
+    font-size: 13px;
+    color: #666666
+  }
+
+  .card-list .footer {
+    background: #F6F6F6;
+    position: absolute;
+    bottom: 0px;
+    left: 0px;
+    right: 0px;
+    border-top: 1px solid #eee;
+    border-radius: 0px 0px 5px 5px;
+  }
+
+  .card-list .footer table {
+    width: 100%;
+    height: 100%;
+  }
+
+  .card-list .footer .status {
+    padding: 3px 0px 3px 15px;
+    color: #fff;
+    display: inline-block;
+    font-size: 10px;
+    width: 60px;
+    margin-right: 20px;
+  }
+
+  .card-list .assign {
+    font-size: 11px;
+    border-left: 1px solid #EEEEEE;
+    border-right: 1px solid #EEEEEE;
+    line-height: 14px;
+    padding: 10px 10px 10px 20px;
+  }
+
+  .popover__content {
+    width: 275px;
+  }
+
+  .wo-detail .btn {
+    width: 100%
+  }
 </style>
